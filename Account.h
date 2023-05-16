e74c17e59677292700c968fae6c7964d13d356ad
ccccy666 <119689373+ccccy666@users.noreply.github.com>
2023-05-16 23:56:31 +0800

    Create Account.h

---
+++ Account.h
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cstring>
+#include "b+ tree.h"
+
+using std::string;
+#ifndef B_TREE_ACCOUNT_H
+#define B_TREE_ACCOUNT_H
+class user_id{
+public:
+    char userid[22];
+    user_id(){};
+    user_id(std::string &in){
+        //if(in.size()>20)printf("-1\n");
+
+        for(int i=0;i<in.size();i++){
+            userid[i]=in[i];
+        }
+        userid[in.size()]='\0';
+    }
+    user_id(const user_id &other){
+        strcpy(userid,other.userid);
+    }
+    bool operator==(const user_id &other)const{
+        return (!strcmp(userid,other.userid));
+    }
+    bool operator!=(const user_id &other)const{
+        return (strcmp(userid,other.userid));
+    }
+    bool operator <(const user_id &other)const{
+        return (strcmp(userid,other.userid)<0);
+    }
+    bool operator <=(const user_id &other)const{
+        return ((strcmp(userid,other.userid)<0)||(!strcmp(userid,other.userid)));
+    }
+    bool operator >=(const user_id &other)const{
+        return !(*this<other);
+        //return ((strcmp(userid,other.userid)<0)||(!strcmp(userid,other.userid)));
+    }
+    bool operator >(const user_id &other)const{
+        return (!(*this<other))&&(!(*this==other));
+        //return ((strcmp(userid,other.userid)<0)||(!strcmp(userid,other.userid)));
+    }
+};
+
+class account{
+public:
+    user_id id;
+    char password[32];
+    char name[12];
+    char email[32];
+    int privilege=0;
+    account(){}
+    account(const account &other):id(other.id){
+        strcpy(password,other.password);
+        strcpy(name,other.name);
+        strcpy(email,other.email);
+        //strcpy(id.userid,other.id.userid);
+        privilege=other.privilege;
+    }
+    ~account(){}
+    bool operator<(const account &other)const{
+        return id<other.id;
+    }
+};
+
+class All_account{
+public:
+    //string file;
+    b_plus_tree<user_id,account> tr;
+    All_account():tr("ac"){};
+    ~All_account(){}
+
+    bool exist(user_id &id){
+        return tr.exist(id);
+    }
+
+    void insert(account &acc){
+        point<user_id,account> p(acc.id,acc);
+        tr.insert(p);
+    }
+    account find(user_id &id){
+
+        return tr.find(id);
+    }
+
+    void mywrite(block<user_id,account> &bk){
+
+        tr.mywrite(bk.place,bk);
+    }
+    block<user_id,account> getblock(user_id &id){
+        return tr.getblock(id);
+    }
+    void clear(){
+        tr.root=-1;
+        tr.total=-1;
+    }
+    void del(){
+
+    };
+
+};
+#endif //B_TREE_ACCOUNT_H
