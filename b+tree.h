c42449ac9b3cff661a60abc798c2fb851cd237e2
ccccy666 <119689373+ccccy666@users.noreply.github.com>
2023-05-22 21:43:37 +0800

    Update b+tree.h

--- b+tree.h
+++ b+tree.h
@@ -88,8 +88,8 @@ struct block {
     int place = 0;
     int parent = -1;
 
-    point<key, T> ele[401];
-    int pos[401];//子树位置
+    point<key, T> ele[51];
+    int pos[51];//子树位置
     block<key,T>(){
         memset(pos,0,sizeof(pos));
 
@@ -129,7 +129,7 @@ struct block {
 template<class key, class T>
 class b_plus_tree {
 public:
-    const int maxsize = 300, minsize = 150,m=301;
+    const int maxsize = 40, minsize = 20,m=41;
     int total=-1;
     sjtu::vector<int> vec;
     sjtu::vector<block<key,T>>ve;
