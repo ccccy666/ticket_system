5821eba3f9df89a77e29f4fcddbfbe2bcd03b7cb
ccccy666 <119689373+ccccy666@users.noreply.github.com>
2023-05-26 17:44:27 +0800

    Update b+tree.h

--- b+tree.h
+++ b+tree.h
@@ -129,7 +129,7 @@ struct block {
 template<class key, class T>
 class b_plus_tree {
 public:
-    const int maxsize = 60, minsize = 30,m=61;
+    const int maxsize = 120, minsize = 60,m=121;
     int total=-1;
     sjtu::vector<int> vec;
     sjtu::vector<block<key,T>>ve;
